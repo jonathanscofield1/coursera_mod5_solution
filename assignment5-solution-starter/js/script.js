303f7e6552c681ce7c0dd1806bdeccaf06e90044
Jonathan Scofield <jonathanscofield1@gmail.com>
2023-07-22 12:27:04 -0500

    reversion

--- assignment5-solution-starter/js/script.js
+++ assignment5-solution-starter/js/script.js
@@ -115,13 +115,13 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"short_name",chosenCategoryShortName.short_name);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"short_name",chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      homeHtmlUrl = insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
