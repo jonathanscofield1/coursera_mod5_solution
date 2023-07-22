38d7053be4790b025fd126f34c17de6018006543
Jonathan Scofield <jonathanscofield1@gmail.com>
2023-07-22 12:20:42 -0500

    Attempt to fix JSON parsing #3

--- assignment5-solution-starter/js/script.js
+++ assignment5-solution-starter/js/script.js
@@ -115,7 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlURL,"short_name",chosenCategoryShortName.short_name);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"short_name",chosenCategoryShortName.short_name);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
